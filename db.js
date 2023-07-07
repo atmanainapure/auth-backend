a18cb209e231646275cede288706b64804644d5e
Atman Ainapure <81010599+atmanainapure@users.noreply.github.com>
2023-07-07 10:32:56 +0530

    added uri to db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 // mongoose.connect(process.env.DATABASE_MONGO);
 const connectMongo =()=>{ 
     console.log("connecting to mongo");
-    mongoose.connect(process.env.DATABASE_MONGO)
+    mongoose.connect('mongodb+srv://atmanainapure:cccZnlEdDdGN64NP@cluster0.uptyu3x.mongodb.net/?authSource=Cluster0&authMechanism=SCRAM-SHA-1')
     console.log(mongoose.connection.readyState);
 }
 module.exports = connectMongo;
